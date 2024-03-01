3d6462c2419bf209d2b42fc0c513696058b355fa
SyS <sreyassreelal@gmail.com>
2024-03-01 18:17:24 +0530

    mimic username suggestion

--- mimic.js
+++ mimic.js
@@ -7,8 +7,9 @@ async function mimc(db, name) {
     let row = await db.all("SELECT message,author from messages where author = ? COLLATE NOCASE and length(message) > 50 ORDER BY RANDOM() LIMIT 1000", [ name ]);
     try {
         if (row && row.length > 0) {
-            
+            let author = row[0].Author;
             messages = row.map(x => x.Message);
+            
             let markov = new MarkovGen({
                 input: messages,
                 minLength: Math.floor(Math.random() * 51)
@@ -16,12 +17,16 @@ async function mimc(db, name) {
             let sentence = markov.makeChain();
             console.log(sentence);
             //console.log(row);
-            return "This is what **" + name + "** sounds like:\n" + sentence.split('\n').map(x => "> " + x).join('\n');
+            return "This is what **" + author + "** sounds like:\n" + sentence.split('\n').map(x => "> " + x).join('\n');
         } else {
-            return "No message found with that author";
+            let row = await db.all("SELECT author from messages where author like ? and length(message) > 50 COLLATE NOCASE GROUP BY author  LIMIT 10", [ "%" + name + "%" ]);
+            let authors = row.map(x => x.Author).join(",");
+    
+            return "No sufficient or lengthy message found with that author or author name is invalid. Did you meant to mimic any of these users instead? ```" + authors + "```";
         }
     } catch (err) {
         console.log(err);
+        console.log(messages)
         return "Sorry, the text I tried to generate was beyond mortal comprehension.";
     }
 
